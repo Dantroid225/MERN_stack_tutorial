774865001c4a9d03083bb9b220f49d04f7454070
Dantroid225 <danhill225@gmail.com>
2023-03-30 21:02:10 -0400

    signup

--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -92,7 +92,8 @@ const authSubmitHandler = async event => { //could use promise/catch
         formData.append('image', formState.inputs.image.value);
 
         const responseData = await sendRequest(
-          process.env.REACT_APP_BACKEND_URL + '/users/signup', 
+          'https://app.clownwolves.com.api/users/signup',
+          //process.env.REACT_APP_BACKEND_URL + '/users/signup', 
           'POST',
           /* this is replaced with formData for image binary data and sets headers
             JSON.stringify({
